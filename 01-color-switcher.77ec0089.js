7b83c59ad1c43e44dd01ec8df38ea701e535bf74
uTatsii <103516808+uTatsii@users.noreply.github.com>
2022-09-12 17:38:42 +0000

    Deploying to gh-pages from @ uTatsii/test@8bae2353c29f59b217807e48c53b062e5ce01acd 🚀

---
+++ 01-color-switcher.77ec0089.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.77ec0089.js.map
